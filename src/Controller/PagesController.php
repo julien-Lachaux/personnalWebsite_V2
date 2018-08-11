70cdcb36c1606275b869cade6457ac10bff01170
julien lachaux <lachauxwebdev@gmail.com>
2018-08-11 17:06:46 +0200

    pipe currentSection avec l'interface dynamiquement

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -24,13 +24,44 @@ class PagesController {
      */
     public function index (Request $request, Environment $twig, RegistryInterface $doctrine) {
         $currentPage = $request->getPathInfo();
-        $currentSection = 1;
+        $currentSection = $doctrine->getRepository(Section::class)->findOneBy([
+            'link' => $currentPage
+        ]);
+
         $profile = $doctrine->getRepository(Profile::class)->getProfile();
         $sections = $doctrine->getRepository(Section::class)->getDisplayed();
         $tools = $doctrine->getRepository(Tool::class)->getDisplayed();
         $skills_groups = $doctrine->getRepository(SkillGroup::class)->findAll();
 
         return new response($twig->render('pages/index.html.twig', [
+            'profile' => $profile[0],
+            'currentPage' => $currentPage,
+            'currentSection' => $currentSection,
+            'sections' => $sections,
+            'tools' => $tools,
+            'skills_groups' => $skills_groups
+        ]));
+    }
+
+    /**
+     * cv
+     *
+     * @Route("/cv")
+     * @param Environment $twig
+     * @return void
+     */
+    public function cv (Request $request, Environment $twig, RegistryInterface $doctrine) {
+        $currentPage = $request->getPathInfo();
+        $currentSection = $doctrine->getRepository(Section::class)->findOneBy([
+            'link' => $currentPage
+        ]);
+
+        $profile = $doctrine->getRepository(Profile::class)->getProfile();
+        $sections = $doctrine->getRepository(Section::class)->getDisplayed();
+        $tools = $doctrine->getRepository(Tool::class)->getDisplayed();
+        $skills_groups = $doctrine->getRepository(SkillGroup::class)->findAll();
+
+        return new response($twig->render('dev/testVars.html.twig', [
             'profile' => $profile[0],
             'currentPage' => $currentPage,
             'currentSection' => $currentSection,
